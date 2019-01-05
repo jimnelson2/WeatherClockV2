7993a7d4e11fa0ddb1161f401639310cb23f871d
Jim Nelson <nelson_dev@hotmail.com>
2019-01-05 15:14:54 -0500

    adding configuration and env vars

--- cmd/weatherclock/weatherclock.go
+++ cmd/weatherclock/weatherclock.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/jimnelson2/WeatherClockV2/pkg/darksky"
+	"github.com/spf13/viper"
+
+	"os"
+
+	"os/signal"
+
+	"syscall"
 )
 
+type runtimeConfig struct {
+	DarkskyToken string
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
+
+	var rc runtimeConfig
+	{
+		viper.SetEnvPrefix("WC")
+		viper.AutomaticEnv()
+
+		// repeat something like this for each config var
+		rc.DarkskyToken = viper.GetString("DARKSKY_TOKEN")
+		if !viper.IsSet("DARKSKY_TOKEN") {
+			fmt.Printf("MISSING DARKSKY_TOKEN\n")
+		}
+	}
+
+	// play nice and try to exit when asked by the system
+	errs := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	// do stuff
+	fmt.Printf("%s\n", rc.DarkskyToken)
 	darksky.Tinker()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/jimnelson2/WeatherClockV2
 
-require github.com/shawntoffel/darksky v1.2.1
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/shawntoffel/darksky v1.2.1
+	github.com/spf13/viper v1.3.1
+)
