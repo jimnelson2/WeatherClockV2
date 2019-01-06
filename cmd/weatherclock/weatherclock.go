af5f973519da5c0b963fbbbd294e1a237772adc3
Jim Nelson <nelson_dev@hotmail.com>
2019-01-05 23:09:28 -0500

    mess around with setting up darksky calls on an interval. do not like

--- cmd/weatherclock/weatherclock.go
+++ cmd/weatherclock/weatherclock.go
@@ -1,59 +1,41 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/jimnelson2/WeatherClockV2/pkg/darksky"
 	"github.com/spf13/viper"
-
-	"os"
-
-	"os/signal"
-
-	"syscall"
 )
-
-// RuntimeConfig will get cleaned up once we settle on context
-type RuntimeConfig struct {
-	DarkskyToken string
-	Latitude     float64
-	Longitude    float64
-}
 
 func main() {
 
-	var rc RuntimeConfig
+	var dsc darksky.Job
+	var darkskyPollSec int
 	{
 		viper.SetEnvPrefix("WC")
 		viper.AutomaticEnv()
 
-		// repeat something like this for each config var
-		rc.DarkskyToken = viper.GetString("DARKSKY_TOKEN")
+		dsc.DarkskyToken = viper.GetString("DARKSKY_TOKEN")
 		if !viper.IsSet("DARKSKY_TOKEN") {
 			panic("MISSING DARKSKY_TOKEN\n")
 		}
 
-		rc.Latitude = viper.GetFloat64("LATITUDE")
+		dsc.Latitude = viper.GetFloat64("LATITUDE")
 		if !viper.IsSet("LATITUDE") {
 			panic("MISSING LATITUDE\n")
 		}
 
-		rc.Longitude = viper.GetFloat64("LONGITUDE")
+		dsc.Longitude = viper.GetFloat64("LONGITUDE")
 		if !viper.IsSet("LONGITUDE") {
 			panic("MISSING LONGITUDE\n")
 		}
 
+		darkskyPollSec = viper.GetInt("DARKSKY_POLL_SEC")
+		if !viper.IsSet("DARKSKY_POLL_SEC") {
+			panic("MISSING DARKSKY_POLL_SEC\n")
+		}
+		if darkskyPollSec < 87 { //TODO make a class constant
+			panic("DARKSKY_POLL_SEC less than 87 will exceed api terms for free use of 1000 calls per day\n")
+		}
 	}
 
-	// play nice and try to exit when asked by the system
-	errs := make(chan error)
-	go func() {
-		c := make(chan os.Signal)
-		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-		errs <- fmt.Errorf("%s", <-c)
-	}()
-
-	// do stuff
-	darksky.Tinker(rc.DarkskyToken, rc.Latitude, rc.Longitude)
-
+	dsc.Run()
 }
--- pkg/darksky/darksky.go
+++ pkg/darksky/darksky.go
@@ -3,19 +3,30 @@ package darksky
 import (
 	"fmt"
 
+	"time"
+
 	"github.com/shawntoffel/darksky"
 )
 
-// Tinker is me just tinkering
-func Tinker(token string, latitude float64, longitude float64) {
-	client := darksky.New(token)
+// Job defines the details for a call to the darksky api
+type Job struct {
+	DarkskyToken string
+	Latitude     float64
+	Longitude    float64
+}
+
+// Run implements the gron.Job interface
+func (dsc Job) Run() {
+	fmt.Println("start darksky at ", time.Now().Format("2006-01-02 15:04:05.000000"))
+	client := darksky.New(dsc.DarkskyToken)
 	request := darksky.ForecastRequest{}
-	request.Latitude = darksky.Measurement(latitude)
-	request.Longitude = darksky.Measurement(longitude)
+	request.Latitude = darksky.Measurement(dsc.Latitude)
+	request.Longitude = darksky.Measurement(dsc.Longitude)
 	forecast, err := client.Forecast(request)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	fmt.Println(forecast.Minutely.Data[0].PrecipType)
+	fmt.Println("next minute precip type: ", forecast.Minutely.Data[0].PrecipType)
+	fmt.Println("finish darksky at ", time.Now().Format("2006-01-02 15:04:05.000000"))
 }
